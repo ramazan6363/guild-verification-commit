a2fe36906fa3269046baba5f1690911561307906
ramazan6363 <140270569+ramazan6363@users.noreply.github.com>
2026-03-15 00:06:14 +0300

    Fix race condition in async operations

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T21:06:10.990Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-14T21:06:13.833Z
+Commit: Fix race condition in async operations
