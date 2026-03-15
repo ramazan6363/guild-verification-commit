b7ec1f5e588f8551768d8d86fd0131261f92bbbc
ramazan6363 <140270569+ramazan6363@users.noreply.github.com>
2026-03-15 06:49:19 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T21:06:17.615Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-15T03:49:18.722Z
+Commit: Patch API endpoint error handling
